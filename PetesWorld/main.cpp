533dd998f738e928869c561ad4466da0a9b73b34
(Student  C00241015) Muirinn Larkin <C00241015@setu.ie>
2023-11-10 10:03:54 +0000

    C Countries

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -22,7 +22,7 @@ int main()
 		std::cout << "You need to select a number between 1 and 5" << std::endl;
 		return 0;
 	}
-	std::cout << "Pick a letter from A to A or Y to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to C or Y to Z" << std::endl; // change this line each iteration
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
 	{ 
@@ -79,6 +79,31 @@ int main()
 			std::cout << "Bahamas, Barbados" << std::endl;
 		}
 	}
+
+	if (letter == 'c' || letter == 'C') //written by Rin
+	{
+		if (region == 1)
+		{
+			std::cout << "Canada, Chile, Colombia, Costa Rica" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "Croatia, Czech Republic" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Cameroon, Cape Verde, Central African Republic, Chad, Comoros, Democratic Republic of the Congo (Kinshasa), Congo, Republic of (Brazzaville), Côte D'ivoire (Ivory Coast)" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "Cambodia, China, Christmas Island, Cocos (Keeling) Islands, Cyprus" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Cayman Islands Greater Antilles, Cook Islands, Cuba" << std::endl;
+		}
+	}
+
 
 	if (letter == 'z' || letter == 'Z') // written by Pete
 	{
