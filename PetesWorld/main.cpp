bc119ddb57d43dac93c6289ddd8f3c2023c069b4
(Student  C00241015) Muirinn Larkin <C00241015@setu.ie>
2023-11-10 09:21:09 +0000

    Name Change

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -1,7 +1,7 @@
 // program to find countries in Pete's World
 // date November 2022
 // author Pete Lowe
-// add your name here
+// Rin Larkin
 //  or here
 // estimated time 120 minutes
 // actual time ??
